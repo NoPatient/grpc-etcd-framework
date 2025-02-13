ede199457900268309b65cf64f3fcb87a43159a1
harvey_hou <harvey_hou@trendmicro.com>
2025-02-13 14:12:39 +0800

    add circuitbreaker and test

--- cfg/config.go
+++ cfg/config.go
@@ -1,3 +1,6 @@
 package cfg
 
-const ETCDEndpoint = "127.0.0.1:2379"
+const (
+	ETCDEndpoint = "127.0.0.1:2379"
+	SERVICENAME  = "MyService"
+)
--- client/client.go
+++ client/client.go
@@ -2,28 +2,19 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"grpc-etcd-framework/cfg"
+	"grpc-etcd-framework/circuitbreaker"
 	pb "grpc-etcd-framework/gen/go/v1"
 	"grpc-etcd-framework/registry"
 	"log"
 	"time"
 )
 
-func callSayHello(reg *registry.Registry, name string) {
-	addrs, err := reg.Discover("MyService")
-	if err != nil {
-		log.Fatalf("failed to discover service: %v", err)
-	}
-
-	if len(addrs) == 0 {
-		log.Fatalf("no available service instances")
-	}
-
+func callSayHello(serviceAddrs []string, name string) {
 	conn, err := grpc.NewClient(
-		addrs[0],
+		serviceAddrs[0],
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 	)
 	if err != nil {
@@ -44,14 +35,64 @@ func callSayHello(reg *registry.Registry, name string) {
 	log.Printf("Greeting: %s", r.Message)
 }
 
-func main() {
-	// Deprecated methods
-	//conn, err := grpc.Dial("localhost:9999", grpc.WithInsecure(), grpc.WithBlock())
+func callSayHelloWithCircuitBreaker(serviceAddrs []string, name string, cb *circuitbreaker.CircuitBreaker) {
+	conn, err := grpc.NewClient(
+		serviceAddrs[0],
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewHelloServiceClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	if !cb.Allow() {
+		log.Printf("Request locked by circuit breaker")
+		return
+	}
+	r, err := client.SayHello(ctx, &pb.HelloRequest{
+		Name: name,
+	})
+	if err != nil {
+		cb.Failure()
+		log.Printf("Call SyaHello failed: %v", err)
+	} else {
+		cb.Success()
+		log.Printf("Call SayHello success: %s", r.Message)
+	}
+}
+
+func getServiceAddresses() []string {
 	etcdEndpoints := []string{cfg.ETCDEndpoint}
-	fmt.Printf("etcdEndpoints: %v\n", etcdEndpoints)
+	log.Printf("etcdEndpoints: %v", etcdEndpoints)
+
 	reg, err := registry.NewRegistry(etcdEndpoints, 50)
 	if err != nil {
 		log.Fatalf("failed to new registry: %v", err)
 	}
-	callSayHello(reg, "Harvey")
+
+	addrs, err := reg.Discover(cfg.SERVICENAME)
+	if err != nil {
+		log.Fatalf("failed to discover service:	%v", err)
+	}
+
+	if len(addrs) == 0 {
+		log.Fatalf("no available service instances")
+	}
+
+	return addrs
+}
+
+func main() {
+	serviceAddrs := getServiceAddresses()
+	callSayHello(serviceAddrs, "Harvey")
+
+	cb := circuitbreaker.NewCircuitBreaker(5, 10*time.Second)
+	for i := 0; i < 20; i++ {
+		callSayHelloWithCircuitBreaker(serviceAddrs, "Harvey Breaker", cb)
+		time.Sleep(100 * time.Millisecond)
+	}
 }
